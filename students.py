a682b804a331bfddb9d0bf8a5b1dda6cafbf1067
David <david.van.steertegem@gmail.com>
2020-09-22 15:40:42 +0200

    added solution for students

---
+++ students.py
@@ -0,0 +1,11 @@
+import random
+
+students = ["John","Karl","Olaf","Zacharia","Elke","Jean-luc","Jean-jean","Kevin","Sidney"]
+
+print("aantal groepen " + str(len(students) // 4 ))
+print("resterende studenten " + str( len(students) % 4 ))
+print(students[4:8] )
+
+print(students[random.randint(0, len(students)-1 )])
+print(random.sample(students, 1))
+print(random.choice(students))
